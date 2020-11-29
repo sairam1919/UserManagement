88c174896fd085ba14443e7bdb93c1951dc978ed
dilipgudivada <dilipgudivada@users.noreply.github.com>
2020-11-29 15:18:38 +0530

    Capture

---
+++ src/containers/dashboard/CapturePhoto/capturePhoto.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+import Webcam from "react-webcam";
+
+export const CapturePhoto = (props) => { 
+  const handleTakePhotoAnimationDone = (dataUri) => {
+    props.handleCapture(dataUri)
+  }
+ 
+  return (
+    <div>
+      {
+        (props.dataUrl)
+          ? <img 
+          src={props.dataUrl}
+          style={{ height: 300 , width: 300}}
+          />
+          : 
+          <div style={{ height: 300 , width: 300}}>
+          <Camera 
+          onTakePhotoAnimationDone = { (dataUri) => { handleTakePhotoAnimationDone(dataUri); } }
+          />
+          </div>
+      }
+    </div>
+  );
+};
+
+
+export class WebcamCapture extends React.Component {
+    setRef = webcam => {
+      this.webcam = webcam;
+    };
+  
+    capture = () => {
+      const imageSrc = this.webcam.getScreenshot();
+    };
+  
+    render() {
+      const videoConstraints = {
+        width: 1280,
+        height: 720,
+        facingMode: "user"
+      };
+  
+      return (
+        <div>
+          <Webcam
+            audio={false}
+            height={350}
+            ref={this.setRef}
+            screenshotFormat="image/jpeg"
+            width={350}
+            videoConstraints={videoConstraints}
+          />
+          <button onClick={this.capture}>Capture photo</button>
+        </div>
+      );
+    }
+  }
--- src/containers/dashboard/GenerateVisitorPassSteps/stepTwo.js
+++ src/containers/dashboard/GenerateVisitorPassSteps/stepTwo.js
@@ -1,15 +1,27 @@
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
-import { TextField, Button } from '@material-ui/core';
-import React from 'react';
+import { Button } from '@material-ui/core';
+import React, { useState} from 'react';
+import {CapturePhoto, WebcamCapture } from '../CapturePhoto/capturePhoto'
 
 const StepTwo = ({handleGeneratePass}) => {
+    // const [ userPhotoUrl, setUserPhotoUrl] = useState('');
+    // const [ idProofPhotoUrl, setIdProofPhotoUrl] = useState('');
+    // const handleUserPhotoCapture = (dataUrl) => {
+    //     setUserPhotoUrl(dataUrl);
+    // }
+
+    // const handleIdProofPhotoCapture = (dataUrl) => {
+    //     setIdProofPhotoUrl(dataUrl);
+    // }
     return (
-        <div>
-            <DialogContent>
-                <DialogContentText style={{ textAlign: 'center' }}>
-                    Image
+        <div style={{ width: 650, overflo: "hidden" }}>
+            <DialogContent >
+                <DialogContentText>
+                   {/* <CapturePhoto dataUrl={userPhotoUrl} handleCapture={handleUserPhotoCapture}/> */}
+                   <WebcamCapture />
+                   {/* <CapturePhoto dataUrl={idProofPhotoUrl} handleCapture={handleIdProofPhotoCapture}/> */}
                 </DialogContentText>
             </DialogContent>
             <DialogActions>
