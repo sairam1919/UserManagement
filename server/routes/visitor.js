8ea73c00d4665ecc447a49e6ec5f70c76ab6c799
dilipgudivada <dilipgudivada@users.noreply.github.com>
2020-12-29 23:27:09 +0530

    idproof and bar code

--- server/routes/visitor.js
+++ server/routes/visitor.js
@@ -6,7 +6,7 @@ const mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Sram@225",
+    password: "password",
     database: "usermanagement",
 });
 
